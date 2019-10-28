05a4c3af52573d63e0e6499f9ca93951af50e47a
Alex Hanson <ahanson@runbox.com>
2019-10-27 19:27:20 -0500

    Update setupscript.sh
    installing apache and apt-mirror

--- setupscript.sh
+++ setupscript.sh
@@ -2,6 +2,7 @@
 # update and upgrade
 sudo apt-get update;sudo apt-get dist-upgrade -y
 # install apache2 and apt-mirror
+sudo apt-get install apache2 apt-mirror
 # Change /etc/hosts
 # Change sources.list
 # Change mirror.list
