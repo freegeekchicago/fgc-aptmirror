e3e06820c3a9df8a1ffb8ae28030f230a721e958
Alex Hanson <ahanson@runbox.com>
2019-10-27 19:18:46 -0500

    First Commit

---
+++ setupscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# update and upgrade
+# install apache2 and apt-mirror
+# Change /etc/hosts
+# Change sources.list
+# Change mirror.list
+# run the apt-mirror script.
+# run the apt-mirror cleanup script.
+# Set up a cron job for the apt-mirror.
+# Set up the redirects.
